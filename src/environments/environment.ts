c77d2842e12aabacf879291ec471548861fc069a
maandeep98 <maandeep998@gmail.com>
2021-02-15 21:25:13 +0530

    test receiving callback

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   clientId: "794632697798492",
   clientSecret: "acf01cebb89b53e43be19b3f8f35306b",
   redirectUri:
-    "https://myigapp.netlify.app/auth/login",
+    "https://myig-app.herokuapp.com/auth/callback",
   token:
     "IGQVJYbEN2a0lOS2tDckoxYW1HX1R6Uml5OFdNT21sMjM5Q3hGYTZAPQUpHV2tXNnlvbFV4ME9lWjRlMXRlRnlLeHJLdFhIcU9RRzJTRXRPOWZAuT2h4LXZAaaUwtakZAlMDRUa015REZA5ZAW9PS2xCRVIwOQZDZD"
 };
